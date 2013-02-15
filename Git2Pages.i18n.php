f9a3056ae42832e5b04107e955ec7ab45773885b
raymond <raimond.spekking@gmail.com>
2013-02-15 09:42:51 +0100

    Add i18n file
    Change-Id: Ic0343939a743b55c72f4c200e54957202be24829

---
+++ Git2Pages.i18n.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Internationalisation file for extension Git2Pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/**
+ * English
+ * @author Teresa Cho
+ * @author Himeshi de Silva
+ */
+$messages['en'] = array(
+	'git2pages-desc' => 'Adds parser function <code><nowiki>{{#snippet:}}</nowiki></code> to add content from a git repositorium into pages',
+);
+
+/** Message documentation (Message documentation)
+ * @author Raymond
+ */
+$messages['qqq'] = array(
+	'git2pages-desc' => '{{desc}}',
+);
--- Git2Pages.php
+++ Git2Pages.php
@@ -6,20 +6,23 @@
 $wgExtensionCredits['parserhook'][] = array(
     'path' => __FILE__,
     'name' => 'Git2Pages',
-    'description' => 'A parser function extension that will add in the contents from a file in a git repo',
+    'descriptionmsg' => 'git2pages-desc',
     'version' => '1.0',
     'author' => array( 'Teresa Cho' , 'Himeshi de Silva' ),
     'url' => 'https://www.mediawiki.org/wiki/Extension:Git2Pages',
 );
 
+$dir = __DIR__;
+
 // Load the extension body to call the static function in the hook
-$wgAutoloadClasses['Git2PagesHooks'] = __DIR__ . '/Git2Pages.body.php';
+$wgAutoloadClasses['Git2PagesHooks'] = $dir . '/Git2Pages.body.php';
 
 // The function that will initialize the parser function
 $wgHooks['ParserFirstCallInit'][] = 'Git2PagesHooks::Git2PagesSetup';
 
-// Allow translation of the parser function name
-$wgExtensionMessagesFiles['Git2Pages'] = __DIR__ . '/Git2Pages.i18n.magic.php';
+// i18n
+$wgExtensionMessagesFiles['Git2Pages'] = $dir . '/Git2Pages.i18n.php';
+$wgExtensionMessagesFiles['Git2PagesMagic'] = $dir . '/Git2Pages.i18n.magic.php';
 
 // Options default values
 $wgGit2PagesDataDir = sys_get_temp_dir();
